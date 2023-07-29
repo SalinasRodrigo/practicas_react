7bdc5c1e29954d4e3d9fd3dc885985f22252356a
Rodrigo Salinas <salinas.e.rodrigo@gmail.com>
2023-07-29 19:05:06 -0400

    Creacion del componente Movies

--- projects/04-practica_useCallback_useMemo_useRef/src/App.jsx
+++ projects/04-practica_useCallback_useMemo_useRef/src/App.jsx
@@ -1,21 +1,21 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import { getMovie } from './services/getMovie'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { getMovie } from "./services/getMovie";
+import { Movies } from "./components/Movies";
 
 //const MOVIE_API_END_POINT= `https://www.omdbapi.com/?apikey=cb7a74b0&s=Pokemon&page=2`
 
 function App() {
-
-  const [movieSearch, setMovieSearch] = useState([])
-  const impt = 'Pokemon' 
-  useEffect(()=>{
+  const [movieSearch, setMovieSearch] = useState([]);
+  const impt = "Pokemon";
+  useEffect(() => {
     //if (!movie) return
-    getMovie(impt).then(newSearch => {
-      console.log(newSearch)
-      setMovieSearch(newSearch)
+    getMovie(impt).then((newSearch) => {
+      console.log(newSearch);
+      setMovieSearch(newSearch);
     });
-    console.log('holas')
-  },[])
+    console.log("holas");
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,28 +24,20 @@ function App() {
 
     const formText = Object.fromEntries(formData.entries());
     console.log(formText);
-  }
+  };
 
   return (
     <main>
       <h1>Busqueda de Peliculas</h1>
       <form method="POST" onSubmit={handleSubmit}>
         <label>
-          Title: <input type="text" name="movieTitle"/>
+          Title: <input type="text" name="movieTitle" />
         </label>
-        <input type="submit"/>
+        <input type="submit" />
       </form>
-      {movieSearch.map( movie =>{
-        return (
-          <div key = {movie.imdbID} >
-            <h2>{movie.Title}</h2>
-            <img src={movie.Poster} alt={`Poster of tehe movie ${movie.Title}`} />
-            <p>Release year: {movie.Year}</p>
-          </div>
-        )
-      })}
+      <Movies movies={movieSearch} />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
