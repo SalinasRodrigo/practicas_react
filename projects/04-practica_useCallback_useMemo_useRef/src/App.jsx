d8b7f5f43953b335980df837391bc55867c50ea3
Rodrigo Salinas <salinas.e.rodrigo@gmail.com>
2023-08-02 17:27:45 -0400

    optimización con useMemo y useCalback, busqueda al escribir con debounce

--- projects/04-practica_useCallback_useMemo_useRef/src/App.jsx
+++ projects/04-practica_useCallback_useMemo_useRef/src/App.jsx
@@ -1,25 +1,38 @@
 //import { useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
 import { useMovies } from "./hooks/useMovies";
 import { useSearch } from "./hooks/useSearch";
+import debounce from "just-debounce-it";
 //const MOVIE_API_END_POINT= `https://www.omdbapi.com/?apikey=cb7a74b0&s=Pokemon&page=2`
 
 function App() {
+  const [sort, setSort] = useState(false);
   const { inpt, setInpt, error } = useSearch();
-  const { movieSearch, movies } = useMovies({ inpt });
+  const { movieSearch, loading, movies } = useMovies({ inpt, sort });
+
+  const debounceMovies = useCallback(
+    debounce(inpt =>{
+      movies({inpt})
+    }, 300)
+  , [movies])
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(event.target.value);
-    movies(inpt);
+    movies({inpt});
   };
 
   const handleChange = (event) => {
     const newInpt = event.target.value;
     if (newInpt.startsWith(" ")) return;
     setInpt(event.target.value);
+    debounceMovies(newInpt)
   };
+
+  const handleSort = () => {
+    setSort(!sort)
+  }
 
   return (
     <div className="page">
@@ -32,12 +45,15 @@ function App() {
             type="text"
             name="movieTitle"
           />
+          <input type="checkbox" onChange={handleSort} checked={sort}/>
           <input type="submit" />
         </form>
         {error && <p className="error ">{error}</p>}
       </header>
       <main>
-        <Movies movies={movieSearch} />
+        {
+        loading ? <p>Loading....</p> : <Movies movies={movieSearch} />
+        }
       </main>
     </div>
   );
--- projects/04-practica_useCallback_useMemo_useRef/src/hooks/useMovies.js
+++ projects/04-practica_useCallback_useMemo_useRef/src/hooks/useMovies.js
@@ -1,14 +1,35 @@
-import { useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 import { getMovie } from "../services/getMovie";
 
-export const useMovies = ({ inpt }) => {
+
+export const useMovies = ({ inpt, sort }) => {
   const [movieSearch, setMovieSearch] = useState([]);
+  const [loading, setLoadin] = useState(false);
+  const [error, setError] = useState(null); 
+  const previusInput = useRef (inpt)
 
-  const movies = () => {
-    getMovie({ inpt }).then((mappedSearch) => {
-      setMovieSearch(mappedSearch);
-    });
-  };
+  const movies = useCallback (async ({inpt}) => {   
 
-  return { movieSearch, movies };
+    if (inpt === previusInput.current) return
+
+    try {
+      setLoadin(true)
+      setError(null)
+      previusInput.current = inpt
+      const newMovies = await getMovie({ inpt })
+      setMovieSearch(newMovies)
+    } catch (e) {
+      setError(e.message)
+    } finally{
+      setLoadin(false)
+    }
+  }, [])
+
+  const sortedMovies = useMemo(() => {
+    return sort
+      ? [...movieSearch].sort((a, b) => a.title.localeCompare(b.title))
+      : movieSearch
+  }, [sort, movieSearch])
+
+  return { movieSearch: sortedMovies, error, loading, movies };
 };
